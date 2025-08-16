cc267389e0ee30880474a7ddeafaf56d40b9a236
Joseph Maynite <joseph.maynite.coi@pcu.edu.ph>
2025-08-16 17:37:34 +0800

    refactor: update Page component to use typed props for better type safety

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,6 +1,11 @@
+type ParamsType = {
+    params: Promise<{
+        productId: string;
+    }>
+}
 
-async function Page({params}) {
-  
+async function Page(props: ParamsType) {
+    const params = await props.params;
     return (
         <div>
             <h1>Product {params.productId}</h1>
