906936316bc2b20cc020994256294b6e86098aca
Joseph Maynite <joseph.maynite.coi@pcu.edu.ph>
2025-12-07 11:22:11 +0800

    updated to nextjs 16 with fixes

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { createServerClient } from "@supabase/ssr";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   try {
     const currentPathname = request.nextUrl.pathname;
     if (currentPathname === "/") {
